16aa30a72e657ecf907ffa1cac5e00d4075138a2
Dccai <deanc2718@gmail.com>
2023-07-18 23:09:03 -0400

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import  { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { FrontPage } from './pages/FrontPage/FrontPage.js';
+import { About } from './pages/About/About.js';
 function App() {
+  function NavBar (){
+    return (
+      <>
+      <nav>
+
+        <div>  <Link to="/">Home</Link></div>
+        <div>  <Link to="/About">About</Link></div>
+        
+      </nav>
+      <Outlet/>
+      </>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route path="/" element={<FrontPage/>}/>
+      <Route path="/About" element={<About/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
